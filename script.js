9afe385e053afcfaafaafd3991becfeae77448ca
hebertluisoficial <162180286+hebertluisoficial@users.noreply.github.com>
2026-01-29 23:35:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".containerPai",
+        start: "top top",
+        end: "+=2000",
+        pin: true,
+        scrub: 2
+    }
+});
+
+tl.to(".secao1", {
+    maskSize: "20vw",
+    duration: 2
+});
+
+tl.to(".logoBg", {
+    opacity: 0,
+    scale: 0.5,
+    duration: 1
+}, "-=1.8");
+
+tl.to(".hebertBg", {
+    filter: "blur(5px)",
+    translateX: "-200px",
+    duration: 1
+}, "-=1.8");
+
+tl.to(".secaoBranca", {
+    backgroundColor: "white",
+    duration: 1
+}, "-=1");
+
+tl.from(".secao2 img", {
+    opacity: 0,
+    filter: "blur(20px)",
+    duration: .5
+});
+
+tl.to(".textoAnimado h3", {
+    translateX: "-150%",
+    opacity: 0,
+    duration: 5
+}, "-=2.5");
+
+tl.to(".secao3", {
+    maskSize: "200vw",
+    duration: 2
+}, "-=1");
+
